b71ec0aa207fa9feb0a0eb878488d7fdaba68483
Manuel4131 <Manual131419@gmail.com>
2019-09-05 20:08:33 +0800

    Convert document's key value.

---
+++ convert_key_val.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+f = open('data','r')
+f2 = open('ss','w')
+l = f.readline()
+
+while l:
+    p=l.strip().split(',')
+    print(p[1].strip())
+    print(p[0])
+    f2.write(p[1]+','+p[0]+'\n')
+    l=f.readline()
